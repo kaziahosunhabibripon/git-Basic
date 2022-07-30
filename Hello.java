71ff45675597fefc341ca365fb12ee8e39e4cd9b
kaziahosunhabibripon <habibkazi92@gmail.com>
2022-07-30 23:20:44 +0600

    Message

--- Hello.java
+++ Hello.java
@@ -2,11 +2,11 @@ public class Hello{
  public static void main(String[]args){
   System.out.println("Vs code is Awesemoe ..");
 
-  display();
- }
 
- public static void display(){
-  System.out.println("Hello ");
  }
+public static void messgae(){
+	System.out.println("Hurrah ! we are able to contribute ");
+}
+ 
 
 }
