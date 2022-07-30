f574d704865a06466047d3bb1b4cd62fbfe10c24
kaziahosunhabibripon <habibkazi92@gmail.com>
2022-07-30 22:54:35 +0600

    New Function added

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
- public static void main(string[]args){
+ public static void main(String[]args){
   System.out.println("Vs code is Awesemoe ..");
+
+  grettings("Kazi Ahosun Habib Ripon");
+ }
+
+ public static void grettings(String name){
+  System.out.println("Welcome " + name);
  }
 }
